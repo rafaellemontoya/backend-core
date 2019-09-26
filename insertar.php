85757d60b9a89b988b543e8d3130fb91a732bc44
Chimpi317 <50777271+Chimpi317@users.noreply.github.com>
2019-09-25 19:28:47 -0500

    Cambios en insertar

--- insertar.php
+++ insertar.php
@@ -2,6 +2,7 @@
 <?php
  header("Access-Control-Allow-Origin: *");
  include ("conexion.php");
+ include ("enviar_mail");
  $datos=$_GET['datos'];
  $datos = json_decode($datos,true);
  // $nombre = $body["nombre"];
@@ -10,24 +11,29 @@
  $empresa = filter_var($datos["empresa"], FILTER_SANITIZE_STRING);
  $telefono = filter_var($datos["telefono"], FILTER_SANITIZE_STRING);
  $taller = filter_var($datos["taller"], FILTER_SANITIZE_STRING);
-insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono);
-$query = "SELECT `id` FROM `talleres_core_2019`";
-$result = mysqli_query($conexion, $query);
-function insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono)
-{
-  $respuesta="Se conecto";
-  $stmt = $connect->prepare("INSERT INTO `talleres_core_2019` (
-    `nombre`, `apellidos`,`empresa`, `puesto`, `email`,`telefono`
-  ) VALUES (?,?,?,?,?,?)");
+insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller);
 
-  $stmt->bind_param("ssssss", $nombre,$apellidos,$empresa,$puesto,$email,$telefono
+function insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller)
+{
+  $id_guardado = 0;
+  $respuesta="Se conecto";
+  $stmt = $connect->prepare("INSERT INTO `registro_core` (
+    `nombre`, `apellidos`,`empresa`, `puesto`, `email`,`telefono`,`id_taller`
+  ) VALUES (?,?,?,?,?,?,?)");
+
+  $stmt->bind_param("ssssssi", $nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller
                             );
 
 
   if($stmt->execute()){
     $respuesta="guardo";
     $estado_respuesta = 1;
-    enviar_mail($nombre, $email, $apellidoPaterno,$apellidoMaterno);
+    $query = "SELECT * FROM `registro_core` Order by id DESC Limit 1 ";
+    $result= mysqli_query($connect, $query);
+    $row= mysqli_fetch_assoc($result);
+    $id_guardado = $row[id];
+
+    //enviar_mail($nombre, $email, $apellidoPaterno,$apellidoMaterno);
 
   }else{
     $respuesta="NI guardo $ciudad, $estado, $cp,$email,$telefono ";
@@ -41,11 +47,12 @@ function insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono)
 
 }
 
-    $nombreRespuesta = $nombre. " ".$apellidoPaterno." ".$apellidoMaterno;
+    $nombreRespuesta = $nombre. " ".$apellidos;
     print json_encode( array(
         'respuesta' => $estado_respuesta,
         'estado_respuesta' => $estado_respuesta,
-        'nombre' => $nombreRespuesta
+        'nombre' => $nombreRespuesta,
+        'id'=>$id_guardado
 
       ));
 
