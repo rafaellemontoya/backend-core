7ef4f2d9ebbb1e9b01b298c54184cf2d32217a6b
Chimpi317 <50777271+Chimpi317@users.noreply.github.com>
2019-09-25 22:04:51 -0500

    versión del 25/09 22pm

--- insertar.php
+++ insertar.php
@@ -1,8 +1,7 @@
-
 <?php
  header("Access-Control-Allow-Origin: *");
  include ("conexion.php");
- include ("enviar_mail");
+ include ("enviar_mail.php");
  $datos=$_GET['datos'];
  $datos = json_decode($datos,true);
  // $nombre = $body["nombre"];
@@ -10,28 +9,35 @@
  $apellidos = filter_var($datos["apellidos"], FILTER_SANITIZE_STRING);
  $empresa = filter_var($datos["empresa"], FILTER_SANITIZE_STRING);
  $telefono = filter_var($datos["telefono"], FILTER_SANITIZE_STRING);
- $taller = filter_var($datos["taller"], FILTER_SANITIZE_STRING);
+ $taller = intval($datos["taller"]);
+ $puesto = filter_var($datos["puesto"], FILTER_SANITIZE_STRING);
+  $email = filter_var($datos["email"], FILTER_SANITIZE_STRING);
 insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller);
 
 function insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller)
 {
   $id_guardado = 0;
   $respuesta="Se conecto";
+  $respuesta="guardo";
+  $estado_respuesta = 1;
+  $connect = new mysqli("localhost", "themytco_userreg", "R3gistr0Ev3ntoS", "themytco_registro_eventos" );
+
   $stmt = $connect->prepare("INSERT INTO `registro_core` (
-    `nombre`, `apellidos`,`empresa`, `puesto`, `email`,`telefono`,`id_taller`
+    `nombre`, `apellido`,`empresa`, `puesto`, `email`,`telefono`,`id_taller`
   ) VALUES (?,?,?,?,?,?,?)");
 
-  $stmt->bind_param("ssssssi", $nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller
-                            );
+
+
+  $stmt->bind_param("ssssssi", $nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller  );
 
 
   if($stmt->execute()){
     $respuesta="guardo";
     $estado_respuesta = 1;
-    $query = "SELECT * FROM `registro_core` Order by id DESC Limit 1 ";
+    $query = "SELECT * FROM `registro_core` Order by id_registro DESC Limit 1 ";
     $result= mysqli_query($connect, $query);
     $row= mysqli_fetch_assoc($result);
-    $id_guardado = $row[id];
+    $id_guardado = $row[id_registro];
 
     //enviar_mail($nombre, $email, $apellidoPaterno,$apellidoMaterno);
 
@@ -45,7 +51,7 @@ function insertar($nombre,$apellidos,$empresa,$puesto,$email,$telefono,$taller)
 
 
 
-}
+
 
     $nombreRespuesta = $nombre. " ".$apellidos;
     print json_encode( array(
